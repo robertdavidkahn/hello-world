b4c9d55f94ffe9ef59e9790a18f2dc7ed17bb8d4
Robert Kahn <robertdavidkahn@gmail.com>
2019-08-17 19:34:28 -0700

    Modified Shape ctor; added Circle and Square classes.

--- Shapes/clsShape.py
+++ Shapes/clsShape.py
@@ -1,8 +1,30 @@
+from abc import abstractmethod
+
 class Shape:
-    def __init__(self):
-        self.value = value
+    def __init__(self, name):
+        self.name_ = name
         super().__init__()
     
     @abstractmethod
     def draw(self):
         pass
+
+    def name(self):
+        return self.name_
+
+#--------------------------------------------
+class Circle(Shape):
+    def __init__(self, name):
+        Shape.__init__(self, "Cirlce_" + name)
+
+    def draw(self):
+        print("Drawing " + Shape.name(self))
+
+
+#--------------------------------------------
+class Square(Shape):
+    def __init__(self, name):
+        Shape.__init__(self, "Square_" + name)
+
+    def draw(self):
+        print("Drawing " + Shape.name(self))
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import sys
+sys.path.insert(0, "Shapes")
+
+import clsShape
+
 print("Hello, world")
+
+aCircle1 = clsShape.Circle("c1")
+aCircle2 = clsShape.Circle("c2")
+
+aSquare1 = clsShape.Circle("sq1")
+
+aCircle1.draw()
+aCircle2.draw()
+aSquare1.draw()
